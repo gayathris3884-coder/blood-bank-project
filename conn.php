ab225bd2384e2cc226181ec627bec4a7cdce5c32
Gayathri S <gayathris3884@gmail.com>
2025-11-05 23:06:38 +0530

    Initial commit: BloodBank project with PostgreSQL support

--- conn.php
+++ conn.php
@@ -1,11 +1,13 @@
 <?php
-$servername = "db"; // Docker service name if using docker-compose
-$username = "bdmsuser";
-$password = "bdms123";
+$host = "YOUR_RENDER_DB_HOST";
+$port = "5432";
 $dbname = "bdms";
+$user = "bdmsuser";
+$password = "PCbF8Ybzrq04GIWz7c9UXyOf6KekAaKe";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . pg_last_error());
 }
 ?>
